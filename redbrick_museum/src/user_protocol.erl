ef9461352c16c7377b4ae870e41cd9566edae88a
Cian Synnott <cian@emauton.org>
2013-03-05 09:57:17 +0100

    Make the echo server do colour escapes; kill timeout.

--- redbrick_museum/src/user_protocol.erl
+++ redbrick_museum/src/user_protocol.erl
@@ -10,9 +10,9 @@ init(ListenerPid, Socket, Transport, _Opts = []) ->
 	loop(Socket, Transport).
 
 loop(Socket, Transport) ->
-	case Transport:recv(Socket, 0, 5000) of
+	case Transport:recv(Socket, 0, infinity) of
 		{ok, Data} ->
-			Transport:send(Socket, Data),
+			Transport:send(Socket, colour:interpolate(binary_to_list(Data))),
 			loop(Socket, Transport);
 		_ ->
 			ok = Transport:close(Socket)
